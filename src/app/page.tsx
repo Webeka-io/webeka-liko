9962aa92a265f6f30bc0f0f284a9caf754331efc
Wylen <webeka.contact@gmail.com>
2025-08-27 17:08:55 +0200

    Mise à jour 

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import HomeFourMain from "@/pages/homes/home-4";
 
 export const metadata: Metadata = {
-  title: "Webeka.io - Créer son site simplement",
+  title: "Webeka.fr - Création de site simple",
 };
 
 export default function Home() {
