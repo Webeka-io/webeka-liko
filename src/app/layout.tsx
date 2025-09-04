153735d357c0bca44a849ee28005b75cd1404f6e
Wylen <webeka.contact@gmail.com>
2025-09-04 13:28:30 +0200

    Mise à jour Analitycs 

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 import "./globals.scss";
 import WhatsAppButton from "@/components/widgets/WhatsAppButton"
+import { Analytics } from "@vercel/analytics/next"
 
 const gellery = localFont({
   src: [
@@ -97,7 +98,7 @@ export default function RootLayout({
         className={`${gellery.variable} ${aladin.variable} ${syne_body.variable} ${syne_heading.variable} ${syne_p.variable} ${syne.variable} ${big_shoulders.variable} ${marcellus.variable}`}
       ><WhatsAppButton phoneE164="+33753343997" />
         <ThemeProvider defaultTheme="light">{children}</ThemeProvider>
-
+        <Analytics />
       </body>
     </html>
   );
