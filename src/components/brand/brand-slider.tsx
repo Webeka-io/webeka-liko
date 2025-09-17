1a3d3895d5c7c5a7cfa7af72aad0ff04db90693b
Wylen <webeka.contact@gmail.com>
2025-09-17 23:48:15 +0200

    Mise à jour

--- src/components/brand/brand-slider.tsx
+++ src/components/brand/brand-slider.tsx
@@ -10,39 +10,23 @@ import b_3 from "@/assets/img/home-01/brand/brand-3.png";
 import b_4 from "@/assets/img/home-01/brand/brand-4.png";
 import b_5 from "@/assets/img/home-01/brand/brand-5.png";
 import b_6 from "@/assets/img/home-01/brand/brand-6.png";
-import b_7 from "@/assets/img/home-01/brand/brand-7.png";
-import b_8 from "@/assets/img/home-01/brand/brand-2.png";
 
 
-const brand_images = [b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8,  b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8];
+const brand_images = [b_1, b_2, b_3, b_4, b_5, b_6, b_1, b_2, b_3, b_4, b_5, b_6];
 
 export default function BrandSlider() {
   return (
     <div className="tp-brand-slider-active fix pt-30">
-      <Marquee
-        speed={100}
-        loop={0}
-        className="brand-wrapper mx-auto"
-      >
+      <Marquee speed={40} loop={0} className="brand-wrapper mx-auto">
         {brand_images.map((b, i) => (
-          <div key={i} className="tp-brand-item" style={{ height: "auto",width: "200px" }}>
-            <Image src={b} alt="" />
+          <div
+            key={i}
+            className={`tp-brand-item h-auto w-auto ${i === 0 ? "ml-16" : ""} mr-16`}
+          >
+            <Image src={b} alt={`brand-${i}`} />
           </div>
         ))}
       </Marquee>
-      {/* <Swiper
-        {...slider_setting}
-        modules={[Autoplay, FreeMode]}
-        className="brand-wrapper"
-      >
-        {brand_images.map((b, i) => (
-          <SwiperSlide key={i}>
-            <div className="tp-brand-item">
-              <Image src={b} alt="" />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper> */}
     </div>
   );
 }
