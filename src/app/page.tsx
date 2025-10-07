9f2365d554c1b5b8f5b618f316ec5bcf3c659254
Wylen <webeka.contact@gmail.com>
2025-10-07 17:48:08 +0200

    Mise à jour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import HomeFourMain from "@/pages/homes/home-4";
+import HomeFourMain from "@/pages/homes/home-dentiste";
 
 export const metadata: Metadata = {
   title: "Webeka.fr - Création de site simple",
