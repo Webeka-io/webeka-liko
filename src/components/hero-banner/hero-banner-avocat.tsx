ed54480ff5739fefddabd0eea8b94bb3d3d2c039
Wylen <webeka.contact@gmail.com>
2025-10-21 22:54:03 +0200

    Mise à jour

--- src/components/hero-banner/hero-banner-avocat.tsx
+++ src/components/hero-banner/hero-banner-avocat.tsx
@@ -9,10 +9,10 @@ import logoJustice from "@/assets/img/avocat/logo_justice.png"
 
 export default function HeroBannerFour() {
   return (
-    <div className="tp-hero-3-area tp-hero-3-ptb fix">
+    <div className=" tp-hero-3-area tp-hero-3-ptb1 fix">
       <div className="container">
         <div className="row">
-          <div className="col-xl-12">
+          <div className="col-xl-12 pt-dynamic">
             <div className="tp-hero-3-content-box text-center p-relative">
               <div className="tp-hero-3-circle-shape my-custom-circle">
                 <span></span>
@@ -20,14 +20,14 @@ export default function HeroBannerFour() {
 
                <h4 className="tp-hero-3-title tp_reveal_anim">
   <span className="tp-reveal-line">Un Site Web</span><br />
-  <span className="tp-reveal-line d-inline-flex align-items-center justify-content-center">
-    Crédible
+  <span className="tp-reveal-line d-inline-flex align-items-center justify-content-center ">
+    Clé&nbsp;en&nbsp;Main
     <span className="logo-wrap d-inline-block position-relative align-middle ms-2">
       <Image
         src={logoJustice}
         alt="Logo sourire"
         fill
-        className="object-fit-contain"
+        className="object-fit-contain handshake-logo"
         sizes="(min-width:1200px) 180px, (min-width:768px) 60px, (min-width:576px) 60px, 40px"
       />
     </span>
@@ -35,23 +35,23 @@ export default function HeroBannerFour() {
 </h4>
 
               <span className=" md : d-none d-md-block tp-hero-3-category tp_reveal_anim ">
-                Site sur-mesure • Respect du RGPD • Sans engagement
+                Design Moderne • Référencement Local • Sans engagement
               </span>
               
             {/* Mobile Avantages */}
-            <div className="d-block d-md-none text-center">
+            <div className="d-block d-md-none text-center pb-30">
   <div className="d-inline-block text-start tp-hero-3-category">
     <div className="d-flex">
       <span className="me-2">•</span>
-      <span>Site premium</span>
+      <span>Design Moderne</span>
+    </div>
+    <div className="d-flex">
+      <span className="me-2">•</span>
+      <span>Référencement Local</span>
     </div>
     <div className="d-flex">
       <span className="me-2">•</span>
       <span>Sans engagement</span>
-    </div>
-    <div className="d-flex">
-      <span className="me-2">•</span>
-      <span>Intégration Doctolib</span>
     </div>
   </div>
 </div>
@@ -61,8 +61,8 @@ export default function HeroBannerFour() {
 
 
           
-              <Link className="tp-btn-black-2" href="/contact">
-                Créer mon site{" "}
+              <Link className="tp-btn-black-2" href="https://calendly.com/webeka-contact/30min?month=2025-10" data-offer-modal>
+                Réserver un appel{" "}
                 <span className="p-relative">
                   <RightArrowTwo />
                   <ArrowBg />
